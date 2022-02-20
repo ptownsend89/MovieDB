171af141b4cbc6340dde569708649d0d15396870
ptownsend89 <84019297+ptownsend89@users.noreply.github.com>
2022-02-20 15:47:35 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         try{
             String tst = "TESTUSER";
             SQLConn con = new SQLConn();
             //con.testConn(tst);
-            System.out.println(con.testConn(tst));
+            if (con.testConn(tst)){
+                System.out.println("Enter new Username:");
+                String inp = sc.nextLine();
+                System.out.println("Now enter password:");
+                String inpPW = sc.nextLine();
+                if (con.createUser(inp,inpPW)){
+                    System.out.println("Success! New user "+inp+" created.");
+                } else{
+                    System.out.println("New user creation failed.");
+                }
+            } else {
+                System.out.println("Connection to database failed.");
+            }
         } catch (Exception e){
             e.printStackTrace(); //print stack trace is method from Exception class
         }
     }
-
-
-
 }
-
-
-
-
-
-
-//MOVIEDB API KEY: 7d36ba51a1f8556d39d8af08af87b75d
-//example API request: https://api.themoviedb.org/3/movie/550?api_key=7d36ba51a1f8556d39d8af08af87b75d
-//API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDM2YmE1MWExZjg1NTZkMzlkOGFmMDhhZjg3Yjc1ZCIsInN1YiI6IjYyMGVhMDgxMzk2ZTk3MDA2YTg2ZDBlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-aIXz2czsUOYd7VX-jV4oLWS1XtK_DA6SVE6Vs2lzws
