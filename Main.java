5ebb5ff0ca95b79168e3a31741d94d0f0cdd99dd
ptownsend89 <84019297+ptownsend89@users.noreply.github.com>
2022-03-11 19:34:45 +0000

    Main minor update
    Testing connections, minor update. Set up for connecting to API and searching database.

--- Main.java
+++ Main.java
@@ -1,31 +1,42 @@
 package com.company;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.sql.SQLException;
+import java.util.Map;
 import java.util.Scanner;
+
+import static com.company.ConnMDB.testConn;
 
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        try{
-            String tst = "TESTUSER";
-            SQLConn con = new SQLConn();
-            //con.testConn(tst);
-            if (con.testConn(tst)){
-                System.out.println("Enter new Username:");
-                String inp = sc.nextLine();
-                System.out.println("Now enter password:");
-                String inpPW = sc.nextLine();
-                if (con.createUser(inp,inpPW)){
-                    System.out.println("Success! New user "+inp+" created.");
-                } else{
-                    System.out.println("New user creation failed.");
-                }
-            } else {
-                System.out.println("Connection to database failed.");
-            }
-        } catch (Exception e){
-            e.printStackTrace(); //print stack trace is method from Exception class
-        }
+    public static void main(String[] args) throws SQLException {
+//        try{
+//            String tst = "TESTUSER";
+//            SQLConn con = new SQLConn();
+//            //con.testConn(tst);
+//            if (con.testConn(tst)){
+//                System.out.println("Enter new Username:");
+//                String inp = sc.nextLine();
+//                System.out.println("Now enter password:");
+//                String inpPW = sc.nextLine();
+//                if (con.createUser(inp,inpPW)){
+//                    System.out.println("Success! New user "+inp+" created.");
+//                } else{
+//                    System.out.println("New user creation failed.");
+//                }
+//            } else {
+//                System.out.println("Connection to database failed.");
+//            }
+//        } catch (Exception e){
+//            e.printStackTrace(); //print stack trace is method from Exception class
+//        }
+        ConnMDB con = new ConnMDB();
+            testConn();
+            System.out.println("Enter search movie: ");
+            con.parseJSON(con.conParams(sc.nextLine()));
     }
+
 }
