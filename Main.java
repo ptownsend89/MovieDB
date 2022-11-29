96f758408217f2a27d8f7cbf5523c17ab83f9bad
ptownsend89 <84019297+ptownsend89@users.noreply.github.com>
2022-11-29 07:33:10 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 package com.company;
-
-import org.json.JSONObject;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.SQLException;
 import java.util.*;
 
-import static com.company.ConnMDB.testConn;
 
 public class Main {
 
@@ -13,46 +13,17 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        System.out.println("Welcome to Movie DB!");
+        try (ServerSocket serverSocket = new ServerSocket(6868)){ 
 
-        String sessionID = "";
-        while (sessionID.equals("") || (sessionID.equals("UNKNOWN"))) {
-            System.out.println("Enter Username:");
-            String user = sc.nextLine();
-            System.out.println("Enter Password");
-            String pass = sc.nextLine();
-            //log in and return session ID if successful:
-            UserAuth auth = new UserAuth(user, pass);
-            try {
-                String authK = auth.authKey();
-                sessionID = auth.logIn(user, pass, authK);
-            } catch (Exception e) {
-                SQLConn errLog = new SQLConn();
-                errLog.errLog(user,e.toString(),"Login failure");
-                System.out.println("Incorrect login details, try again.");
+            while (true){
+                Socket socket = serverSocket.accept();
+                ServerThread s = new ServerThread(socket);
+                System.out.println("ServerThread ID: " + s.getId());
+                s.start();
             }
-        }
-
-
-        //logIn will return "UNKNOWN" string if session ID not found:
-        if (!Objects.equals(sessionID, "UNKNOWN") && !Objects.equals(sessionID, "")){
-            ConnMDB con = new ConnMDB();
-            testConn();
-            System.out.println("Enter search movie: ");
-            ArrayList<String> watchList = new ArrayList<>();
-            watchList = con.parseJSON(con.conParams(sc.nextLine()));
-
-            //Select a film to save:
-            System.out.println("Select a film to save:");
-            int listChoice = sc.nextInt();
-
-            //create json object from selected film option
-            JSONObject filmChoice = new JSONObject(watchList.get(listChoice -1));
-            System.out.println("SELECTED: " +filmChoice.getString("overview"));
-
-            //get film ID from json object key and call saveWatchList to post
-            String choiceID = filmChoice.get("id").toString();
-            con.saveWatchlist(sessionID,choiceID);
+        } catch (IOException io) {
+            System.out.println("Server IO exception " + io.getMessage());
+            io.printStackTrace();
         }
     }
 
