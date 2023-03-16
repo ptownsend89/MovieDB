8085e702af239c058c34c9fe43c1c03d92eb0b05
ptownsend89 <84019297+ptownsend89@users.noreply.github.com>
2023-03-16 17:42:41 +0000

    Update Main.java
    Remove key

--- Main.java
+++ Main.java
@@ -29,9 +29,3 @@ public class Main {
 
 }
 
-
-
-
-//MOVIEDB API KEY: 7d36ba51a1f8556d39d8af08af87b75d
-//example API request: https://api.themoviedb.org/3/movie/550?api_key=7d36ba51a1f8556d39d8af08af87b75d
-//API read access token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDM2YmE1MWExZjg1NTZkMzlkOGFmMDhhZjg3Yjc1ZCIsInN1YiI6IjYyMGVhMDgxMzk2ZTk3MDA2YTg2ZDBlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-aIXz2czsUOYd7VX-jV4oLWS1XtK_DA6SVE6Vs2lzws
